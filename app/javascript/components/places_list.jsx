ebfcaea5586e64166fa3696007e6e75927505d47
ajtheengineer <you@example.com>
2022-06-10 07:14:17 -0700

    Update places_list.jsx to use className, and remove clip-rule, etc
    This addresses all warnings shown in the console. This will
    help me debug more easily.

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -3,23 +3,23 @@ import ReactDOM from "react-dom/client";
 
 function renderPlacesPage(body) {
   return (
-    <div class="bg-white p-8 rounded-md w-full">
-      <div class="flex items-center justify-between pb-6">
+    <div className="bg-white p-8 rounded-md w-full">
+      <div className="flex items-center justify-between pb-6">
         <div>
-          <h2 class="text-4xl text-gray-600 font-semibold">Places</h2>
+          <h2 className="text-4xl text-gray-600 font-semibold">Places</h2>
         </div>
-        <div class="flex items-center justify-between">
-          <div class="flex bg-gray-50 items-center p-2 rounded-md">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+        <div className="flex items-center justify-between">
+          <div className="flex bg-gray-50 items-center p-2 rounded-md">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
               fill="currentColor">
-              <path fill-rule="evenodd"
+              <path
                 d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clip-rule="evenodd" />
+                />
             </svg>
             <input class="bg-gray-50 outline-none ml-1 block w" type="text" name="" id="" placeholder="search..." />
           </div>
-          <div class="lg:ml-40 ml-10 space-x-8">
-            <button class="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">New Log</button>
+          <div className="lg:ml-40 ml-10 space-x-8">
+            <button className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">New Log</button>
           </div>
         </div>
       </div>
@@ -46,14 +46,20 @@ function PlacesList() {
         setLoadedPlaces(data["places"])
         setLoading(false)
       });
-  }, [])
+  }, [searchTerm])
+
+  const onSearchTextChange = (e) =>  {
+    console.log("onSearchTextChange was executed!")
+    setLoading(true);
+    setSearchTerm(e.target.value);
+  }
 
   const loadingSection = (<div>Loading...</div>)
   const tableHeaderClass = "px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
   const dataSection = (
 		<div>
-			<div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-				<div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+			<div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+				<div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
           <table className="min-w-full leading-normal">
             <thead>
               <tr>
@@ -68,28 +74,28 @@ function PlacesList() {
               {loadedPlaces.map((place, index) => {
                 return (
                   <tr key={index}>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <div class="flex items-center">
-                        <div class="ml-3">
-                          <p class="text-gray-900 whitespace-no-wrap">{place.name}</p>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <div className="flex items-center">
+                        <div className="ml-3">
+                          <p className="text-gray-900 whitespace-no-wrap">{place.name}</p>
                         </div>
                       </div>
                     </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">{place.city}</p>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">{place.city}</p>
                     </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">
                         {place.most_recent_download_speed}
                       </p>
                     </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">
                         {place.most_recent_download_units}
                       </p>
                     </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">
                         {place.number_of_measurements}
                       </p>
                     </td>
